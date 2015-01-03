e7422d9e84913d87d9f938682c89ed32fb34ac7f
roman1pr <iroman.pri@gmail.com>
2015-01-03 20:33:33 +0300

    Initial Commit

---
+++ 1/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  1
+//
+//  Created by Роман Приискалов on 03/01/15.
+//  Copyright (c) 2015 Роман Приискалов. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
